ae86453d1733985572e78967a1a8a06643aa5348
Suyara Farias <suyarafarias76@gmail.com>
2026-05-20 21:24:43 -0400

    projeto gc

---
+++ src/main/java/com/exemplo/Conversor.java
@@ -0,0 +1,11 @@
+package com.exemplo;
+
+public class Conversor {
+    public double fahrenheitParaCelsius(double f) {
+        return (f - 32) * 5 / 9;
+    }
+
+    public double celsiusParaFahrenheit(double c) {
+        return (c * 9 / 5) + 32;
+    }
+}
---
+++ src/test/java/com/exemplo/ConversorTest.java
@@ -0,0 +1,18 @@
+package com.exemplo;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ConversorTest {
+    @Test
+    public void testFahrenheitParaCelsius() {
+        Conversor conv = new Conversor();
+        assertEquals(0.0, conv.fahrenheitParaCelsius(32), 0.01);
+    }
+
+    @Test
+    public void testCelsiusParaFahrenheit() {
+        Conversor conv = new Conversor();
+        assertEquals(32.0, conv.celsiusParaFahrenheit(0), 0.01);
+    }
+}
