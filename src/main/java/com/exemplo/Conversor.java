d8161c6d4734f7faf599ce9713f39386cea03ebb
Suyara Farias <suyarafarias76@gmail.com>
2026-05-25 17:00:45 -0400

    gerando falha - caso de teste 2

--- src/main/java/com/exemplo/Conversor.java
+++ src/main/java/com/exemplo/Conversor.java
@@ -2,10 +2,10 @@ package com.exemplo;
 
 public class Conversor {
     public double fahrenheitParaCelsius(double f) {
-        return (f - 32) * 5 / 9
+        return (f - 32) * 5 / 9;
     }
 
     public double celsiusParaFahrenheit(double c) {
-        return (c * 9 / 5) + 32;
+        return (c * 9 / 5) + 32
     }
 }
